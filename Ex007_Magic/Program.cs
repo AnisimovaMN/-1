366693e1f845c0ea028603672b9d3910b5f196de
Marina <dgoun16@mail.ru>
2022-08-19 20:07:03 +0300

    изменили хс

--- Ex007_Magic/Program.cs
+++ Ex007_Magic/Program.cs
@@ -1,21 +1,21 @@
 ﻿Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
-int xa = 40;
-int ya = 1;
+int xa = 45;
+int ya = 5;
 int xb = 1;
 int yb = 30;
-int xc = 80;
+int xc = 90;
 int yc = 30;
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+//Console.SetCursorPosition(xa, ya);
+//Console.WriteLine("+");
+//Console.SetCursorPosition(xb, yb);
+//Console.WriteLine("+");
+//Console.SetCursorPosition(xc, yc);
+//Console.WriteLine("+");
 int x = xa, y = xb;
 int count = 0;
-while (count <1000)
+while (count <10000)
 {
     int what = new Random().Next(0, 3); // 0 1 2 
     if (what == 0)
